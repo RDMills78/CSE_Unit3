b41107cf675f65b5d988eeb556f01f0a0e96c840
rdmills <pltwcybermt@gmail.com>
2023-05-03 10:20:11 -0600

    final code for 3.1.1

---
+++ 311_2023.py
@@ -0,0 +1,14 @@
+# first_python_version1
+print("I am coding in Python")
+# This is a single line comment
+"""This is a multiple line comment
+you can use it for 
+larger blocks of comments"""
+
+def text_prompt():
+    name = input("What is your name?  ")
+    print("Hello  " + name)
+
+text_prompt()
+
+
